24532ea7ade659b03b275e2d82855ed22e1df340
Plant Lady <codeplantsbylouie@gmail.com>
2019-06-11 11:31:33 -0400

    changed const to let and 100 to 101; const is a variable that cannot be altered and 100 was not included in less than, can either be less than or equal to or less than 101

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // declare your variable i and set it equal to 1
-const i = 1;
+let i = 1;
 // use a while loop with a conditional statement of i < 100
-while (i < 100) {
+while (i < 101) {
   // you want to see the numbers in the console
   console.log(i);
   // but i will always be less than 100 so you need to increment it
