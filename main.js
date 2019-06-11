ba7100ea9426fd4f9783f9b60c3ff2aa6d26091e
Plant Lady <codeplantsbylouie@gmail.com>
2019-06-11 11:59:57 -0400

    declared new variables x and y to be placedholders for values that may change (numbers not 3 and 5)

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 // declare your variable i and set it equal to 1
 let i = 1;
+
+// BUT what happens if the parameters of the exercise change?
+// e.g. we don't want 3 and 5 anymore; we want 4 and 9
+// it would be annoying to go through all of the code
+// and if this were a bigger project it would take a long time to go through every single line
+// so, let's make them variables!
+let x = 4;
+let y = 9;
+
 // use a while loop with a conditional statement of i < 100
 while (i < 101) {
   // need to put this stricter condition first or else only the %3 and %5 will register 
-  if (i%3 === 0 && i%5 === 0) {
+  if (i%x === 0 && i%y === 0) {
     console.log("FizzBuzz");
   }
-  else if (i%5 === 0) {
+  else if (i%y === 0) {
     console.log("Buzz");
   }
-  else if (i%3 === 0) {
+  else if (i%x === 0) {
     console.log("Fizz");
   }
   else {
